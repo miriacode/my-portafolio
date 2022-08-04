fd8a26d0ab7bd7dabacb722cce4fc09c0ad2b43b
miriacode <miriamacunaenciso@gmail.com>
2022-08-04 15:56:09 -0500

    Deleting some files

--- src/data/u.js
+++
@@ -1,6 +0,0 @@
-useEffect(() => { 
-    const hash = props.history.location.hash 
-    // Check if there is a hash and if an element with that id exists 
-    const el = hash && document.getElementById(hash.substr(1)) 
-    if (el) { el.scrollIntoView({behavior: "smooth"}) } }, [props.history.location.hash]) 
-    // Fires every time hash changes
