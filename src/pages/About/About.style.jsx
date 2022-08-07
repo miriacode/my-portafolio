d6e98d2c8e7102f6cc690290193ec22ea3ef92aa
miriacode <miriamacunaenciso@gmail.com>
2022-08-06 23:16:18 -0500

    About styles updated

--- src/pages/About/About.style.jsx
+++ src/pages/About/About.style.jsx
@@ -3,13 +3,10 @@ import { keyframes } from "styled-components";
 
 export const AboutSection = styled.section`
     display:block;
-    /* background-color:pink; */
-    padding-top:80px;
+    padding-top:100px;
 `;
 
-
 const gradient = keyframes`
-
 0% {
   background-position: 0 50%;
 }
@@ -25,7 +22,7 @@ export const Title = styled.h2`
     display:block;
     padding:1rem;
     font-size:xx-large;
-   
+    //Gradient Animated Text
     animation: ${gradient} 5s ease-in-out infinite;
     background: linear-gradient(to right, #ee9ca7, #ffdde1, #2193b0, #6dd5ed);
     background-size: 300%;
@@ -35,9 +32,24 @@ export const Title = styled.h2`
 `;
 
 export const Paragraph = styled.p`
-    /* background-color:purple; */
-    padding-top:80px;
-
     height:80vh;
     padding:1.8rem 1rem;
+    font-size:var(--paragraph-size-320);
+    background-color:peru;
+
+    @media (min-width: 500px) {
+      height:auto;
+    }
+    @media (min-width: 700px) {
+      font-size:var(--paragraph-size-700);
+    }
+    @media (min-width: 1200px) {
+      font-size:var(--paragraph-size-1200);
+      padding:1rem 18rem;
+    }
+    @media (min-width: 1800px) {
+      font-size:var(--paragraph-size-1800);
+      padding:1rem 25rem;
+    }
+    
 `;
